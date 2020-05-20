edd5093e60b46887b3ab26ba40d598d6aaf190b3
Luna Comerford <luna.comerford@gmail.com>
2020-05-19 22:21:57 -0700

    Initial Commit
    Created single page app from Xcode Template

---
+++ Upcoming Events/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Upcoming Events
+//
+//  Created by Luna Comerford on 5/19/20.
+//  Copyright © 2020 Luna Comerford. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
